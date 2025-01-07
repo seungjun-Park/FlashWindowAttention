57d81424b4c1e642e5235d1c46663fcb2c88650b
zzd1992 <zzd775089697@163.com>
2025-01-07 21:09:23 +0800

    fw kernel

--- flash_swin_attn/func_flash_swin.py
+++ flash_swin_attn/func_flash_swin.py
@@ -3,7 +3,7 @@ import torch
 import triton
 
 
-def flash_swin_attn_fwd_func(Q, K, V, bias, scale_qk, head, patch_h, patch_w, shift_h, shift_w):
+def flash_swin_attn_fwd_func(Q, K, V, bias, scale_qk, head, patch_h, patch_w, shift_h, shift_w, head_chunk=1):
     batch, img_h, img_w, c = Q.size()
     head_dim = c // head
     O = torch.empty_like(Q)
@@ -11,7 +11,6 @@ def flash_swin_attn_fwd_func(Q, K, V, bias, scale_qk, head, patch_h, patch_w, sh
 
     # deal with shift
     delta_h = 0 if shift_h == 0 else patch_h
-    delta_w = 0 if shift_w == 0 else patch_w
     grid = (batch * head, triton.cdiv(img_h + delta_h, patch_h), 1)
     _window_fwd_kernel[grid](
         Q,
@@ -25,8 +24,8 @@ def flash_swin_attn_fwd_func(Q, K, V, bias, scale_qk, head, patch_h, patch_w, sh
         img_w,
         head,
         head_dim,
-        2,
-        head_dim // 2,
+        head_chunk,
+        head_dim // head_chunk,
         patch_h,
         patch_w,
         shift_h,
